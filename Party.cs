7431f3f6c23e54ae2b85a57d241c6f939276fc95
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-03-27 12:47:28 -0500

    SOME LIKE TO IMAGINE

--- Party.cs
+++ Party.cs
@@ -2,4 +2,18 @@
 public class Party
 {
     // Add your code here
+    public class Person {
+        public string firstName;
+        public string lastName;
+    }
+
+    public Guest (string firstName, string lastName) {
+        this.firstName = "Jimmy";
+        this.lastName = "Scott";
+        this.firstName = "Bobby";
+    }
 }
+    
+
+//should define a class named Person that takes two parameters: firstName and lastName when it's created
+//should create a class-level variable array named Guests that contains three instances of the Person class
