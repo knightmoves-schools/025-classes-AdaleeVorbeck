ab00baeb521f23fb0fce30a233c6b693d6b9d8cb
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-04-10 12:28:08 -0500

    I'm sadly not listening to music today but I can defininelty come up with something, trust

--- Party.cs
+++ Party.cs
@@ -1,19 +1,22 @@
 namespace knightmoves;
 
-public class Party {
-    public Person[] Guests = new Person[];
+public class Person {
+    public string firstName;
+    public string lastName;
 
-    public class Person {
-        public string firstName;
-        public string lastName;
-
-        public Person(string firstName, string lastName) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
+    public Person(string firstName, string lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 }
-    
+
+public class Party {
+    public Person[] Guests = new Person[} {
+        new Person("Jimmy", "Scott"),
+        new Person("Bobby", "Jones"),
+        new Person("Sarah", "Smith")
+    };
+}
 
 //should define a class named Person that takes two parameters: firstName and lastName when it's created
 //should create a class-level variable array named Guests that contains three instances of the Person class
